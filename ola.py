d42b69fc2972aad169334b3c0be44b3e41428a23
Laura Menegazzo Boeing <l.boeing@escola.pr.gov.br>
2026-02-09 14:11:23 +0000

    aula1

---
+++ ola.py
@@ -0,0 +1,8 @@
+print("Laurete")
+nome = input("Digite seu nome: ")
+print("Olá,",nome, "Seja bem vindo(a)")
+
+print("Seu nome é",nome)
+
+sobrenome = input("Qual é seu sobrenome? ")
+print("Seu sobrenome é",sobrenome)
